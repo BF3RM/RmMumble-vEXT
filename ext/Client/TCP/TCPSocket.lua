101885b0f123239d2d57e57344a88444578acdae
FoolHen <fulgen_3_7ad@hotmail.com>
2020-04-06 13:01:15 +0200

    Removed print spam.

--- ext/Client/TCP/TCPSocket.lua
+++ ext/Client/TCP/TCPSocket.lua
@@ -41,7 +41,7 @@ end
 
 function TCPSocket:SetupSocket()
     if not self:AttemptConnection() then
-        print('Retrying in 5 seconds...')
+        print('Coulnd\'t connect to mumble. Is mumble up and running? Retrying in 5 seconds...')
     end
 end
 
@@ -57,14 +57,12 @@ end
 
 function TCPSocket:AttemptConnection()
     local connectResult = self.socket:Connect('127.0.0.1', 64304)
-    print(connectResult)
     if connectResult == 0 then
         print('Connected to mumble')
         self.socketOpen = true
         self.reconnectionDelta = 0.0
         return true
     elseif connectResult == 10035 then -- connecting
-        print('Coulnd\'t connect to mumble. Is mumble up and running?')
         self.isConnecting = false
         return false
     elseif connectResult == 10022 then --  WSAEINVAL
